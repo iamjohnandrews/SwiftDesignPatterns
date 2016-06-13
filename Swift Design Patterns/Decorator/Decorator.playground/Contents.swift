84703c164a7145720540bb3dc47e8010dafabaf5
Andrews, John L <John.L.Andrews@disney.com>
2016-06-13 12:47:52 -0700

    refactor decorator patter to take advantage of protocol extensions

--- Swift Design Patterns/Decorator/Decorator.playground/Contents.swift
+++ Swift Design Patterns/Decorator/Decorator.playground/Contents.swift
@@ -8,7 +8,7 @@ protocol BeverageDecorator {
     func getDrinkCost() -> Double
 }
 
-
+/*
 class Beverage: BeverageDecorator { //treat this like abstract class
     var description = "TBD by Subclass"
     var cost: Double = 0.0
@@ -38,24 +38,7 @@ class HouseBlend: Beverage {
     }
 }
 
-class Expresso: Beverage {
-    override init() {
-        super.init()
-        description = "Expresso"
-        cost = 3.09
-    }
-    
-    override func drinkDescription() -> String {
-        return description
-    }
-    
-    override func getDrinkCost() -> Double {
-        return cost
-    }
-}
-
-
-// Condiment Wrappers
+//Condiment Wrapper
 class Hazelnut: Beverage {
     var beverage: Beverage
     
@@ -71,36 +54,86 @@ class Hazelnut: Beverage {
         return beverage.getDrinkCost() + 0.42
     }
 }
+*/
 
-class Caramel: Beverage {
-    var beverage: Beverage
-    
-    init(chosenDrink: Beverage ) {
-        beverage = chosenDrink
+protocol Beverage: BeverageDecorator {
+    var cost: Double {get set}
+    var description: String {get set}
+}
+
+extension Beverage {
+    func drinkDescription() -> String {
+        return description
     }
-    
-    override func drinkDescription() -> String {
-        return beverage.drinkDescription() + " + Caramel"
-    }
-    
-    override func getDrinkCost() -> Double {
-        return beverage.getDrinkCost() + 0.27
+    func getDrinkCost() -> Double {
+        return cost
     }
 }
 
-class WhipCream: Beverage {
+class HouseBlend: Beverage {
+    var description: String = "House Blend Coffee"
+    var cost: Double = 1.89
+    
+    func drinkDescription() -> String {
+        return description
+    }
+    
+    func getDrinkCost() -> Double {
+        return cost
+    }
+}
+
+class Expresso: Beverage {
+    var description: String = "Expresso"
+    var cost: Double = 3.09
+    
+    func drinkDescription() -> String {
+        return description
+    }
+    
+    func getDrinkCost() -> Double {
+        return cost
+    }
+}
+
+
+// Condiment Wrappers
+struct Hazelnut: Beverage {
+    
     var beverage: Beverage
+    var description: String
+    var cost: Double
     
     init(chosenDrink: Beverage ) {
         beverage = chosenDrink
+        description = beverage.drinkDescription() + " + Hazelnut"
+        cost = beverage.getDrinkCost() + 0.42
     }
+}
+
+struct Caramel: Beverage {
     
-    override func drinkDescription() -> String {
-        return beverage.drinkDescription() + " + Whip Cream"
+    var beverage: Beverage
+    var description: String
+    var cost: Double
+    
+    init(chosenDrink: Beverage ) {
+        beverage = chosenDrink
+        description = beverage.drinkDescription() + " + Caramel"
+        cost = beverage.getDrinkCost() + 0.27
     }
+}
+
+struct WhipCream: Beverage {
     
-    override func getDrinkCost() -> Double {
-        return beverage.getDrinkCost() + 0.43
+    var beverage: Beverage
+    var description: String
+    var cost: Double
+    
+    init(chosenDrink: Beverage ) {
+        beverage = chosenDrink
+        description = beverage.drinkDescription() + " + Whip Cream"
+        cost = beverage.getDrinkCost() + 0.43
     }
 }
 
@@ -109,14 +142,16 @@ houseBlendCoffee.getDrinkCost()
 houseBlendCoffee.drinkDescription()
 
 let hazelnutCoffee = Hazelnut(chosenDrink: houseBlendCoffee)
-hazelnutCoffee.getDrinkCost()
-hazelnutCoffee.drinkDescription()
+hazelnutCoffee.cost
+hazelnutCoffee.description
 
 let hazelnutCaramelCoffee = Caramel(chosenDrink: hazelnutCoffee)
-hazelnutCaramelCoffee.getDrinkCost()
-hazelnutCaramelCoffee.drinkDescription()
+hazelnutCaramelCoffee.cost
+hazelnutCaramelCoffee.description
 
 let expresso = Expresso()
 let caramelHazelNutWhipCreamExpresso = WhipCream(chosenDrink: Caramel (chosenDrink: Hazelnut(chosenDrink: expresso)))
 caramelHazelNutWhipCreamExpresso.getDrinkCost()
 caramelHazelNutWhipCreamExpresso.drinkDescription()
+caramelHazelNutWhipCreamExpresso.cost
+caramelHazelNutWhipCreamExpresso.description
